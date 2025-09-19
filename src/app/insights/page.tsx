f555a1adb29280df256ef7feeb6d8f902b770cd6
AshutoshYadav08 <ashu2017ccyadav@gmail.com>
2025-09-20 01:41:39 +0530

    footer placed

--- src/app/insights/page.tsx
+++ src/app/insights/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import BlogList from "@/components/blog/BlogList";
 import RecentPosts from "@/components/blog/RecentPosts";
+import Footer3 from "@/components/footernew/Footer3";
 
 
 
@@ -12,6 +13,7 @@ export default function Page(){
         <>
         <RecentPosts/>
         <BlogList/>
+        <Footer3/>
         </>
     )
 
